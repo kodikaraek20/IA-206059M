c2cac1455deaeb7cf6a2ee13e889f92a6e4cb8ff
Madhushi Kodikara <147620753+MadhushiKodikara@users.noreply.github.com>
2023-10-15 11:04:53 +0530

    Update Sharing file.py
    I expand on the existing code to make it more interactive and informative. One way to enhance it is to provide additional information about the animal when it's classified. Here's an extended version of your code that includes more details about the animals:

--- Sharing file.py
+++ Sharing file.py
@@ -18,3 +18,48 @@ else:
     print(f"Sorry, I don't know the category for {animal_name}.")
 
 # You can continue to add more animals and categories to the dictionary as needed.
+# Define a dictionary with animal names, their categories, and additional information
+animal_info = {
+    "cat": {
+        "category": "mammal",
+        "sound": "Meow",
+        "habitat": "Domestic and wild",
+    },
+    "dog": {
+        "category": "mammal",
+        "sound": "Woof",
+        "habitat": "Domestic and wild",
+    },
+    "parrot": {
+        "category": "bird",
+        "sound": "Squawk",
+        "habitat": "Tropical regions",
+    },
+    "snake": {
+        "category": "reptile",
+        "sound": "Hiss",
+        "habitat": "Various environments",
+    },
+    "fish": {
+        "category": "fish",
+        "sound": "None",
+        "habitat": "Aquatic environments",
+    }
+}
+
+# Prompt the user to enter the name of an animal
+animal_name = input("Enter the name of an animal: ")
+
+# Classify and provide information about the entered animal
+if animal_name in animal_info:
+    info = animal_info[animal_name]
+    category = info["category"]
+    sound = info["sound"]
+    habitat = info["habitat"]
+    print(f"{animal_name} is a {category}. It makes the sound '{sound}' and is found in {habitat}.")
+else:
+    print(f"Sorry, I don't know the category for {animal_name}.")
+
+# You can continue to add more animals and their information to the dictionary as needed.
+
+
